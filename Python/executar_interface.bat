ee109d6a82cde963cc03634d40cf51761cfd8173
ViniiciusDS <dourado.vinicius@uel.br>
2024-03-04 00:06:33 -0300

    Ambiente virtual executavel

--- Python/executar_interface.bat
+++ Python/executar_interface.bat
@@ -1,9 +1,16 @@
 @echo off
-REM Ativar o ambiente virtual
-call "C:\Users\viniv\Desktop\engenhariaeletrica\TCC_ViniciusDourado\Python\TCC_viniciusD\Scripts\activate.bat"
 
-REM Executar o script da interface gráfica
-python "C:\Users\viniv\Desktop\engenhariaeletrica\TCC_ViniciusDourado\Python\interface_grafica_teste.py"
+rem Detecta o diretório atual do script .bat
+set "script_dir=%~dp0"
 
-REM Desativar o ambiente virtual
+rem Caminho para o ambiente virtual
+set "venv_dir=%script_dir%TCC_viniciusD"
+
+rem Ativa o ambiente virtual
+call "%venv_dir%\Scripts\activate"
+
+rem Executa o script da interface gráfica
+python "%script_dir%interface_grafica_teste.py"
+
+rem Desativa o ambiente virtual
 deactivate
