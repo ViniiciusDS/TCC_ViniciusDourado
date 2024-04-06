84b42b95ad6d983c2c335f4597530683a7f73f75
ViniiciusDS <dourado.vinicius@uel.br>
2024-04-06 16:24:33 -0300

    Backup Troca PC

--- Python/Calc_func.py
+++ Python/Calc_func.py
@@ -1,23 +1,5 @@
 """Funções de cálculo de custo e distância"""
 
-"""Função para calcular o custo, com distâncias e tempo"""
-
-
-def Calc_Custo(DistMatriz, Populacao, TempMatriz):
-    custo = 0
-    cidade_ant = Populacao[0]
-
-    for i in range(1, len(Populacao)):
-        cidade_atual = Populacao[i]
-        custo = custo + \
-            DistMatriz[cidade_ant][cidade_atual] / \
-            TempMatriz[cidade_ant][cidade_atual]
-        cidade_ant = cidade_atual
-
-    Percurso = custo
-    return Percurso
-
-
 """Função para calcular a distância (usada no AG)"""
 
 
